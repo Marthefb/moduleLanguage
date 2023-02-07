a6cebb4be5c17547e5f4c36f229ce2d81e30f56b
Marthefb <marthefb@gmail.com>
2023-02-07 13:11:35 +0100

    norsk oppdatert

--- Dictionary.mjs
+++ Dictionary.mjs
@@ -1,4 +1,32 @@
+import language from "./lan/no.json" assert {type: 'json'};
+
+
 class Dictionary{
 
-    
+    constructor(...languages){
+        this.languages = languages
+        this.dictionary = null;
+        //sier hvilke språk som er tilgjengelige
+    }
+
+    setLanguage = function(){
+        //VElger og laster språket som skal brukes
+    }
+
+    get = function(key){
+        // henter riktig verdi basert på key i Dictionary
+    }
+
+    keys= function(){
+        //returnerer alle keys
+        return Object.keys(this.dictionary);
+    }
 }
+
+DICTIONARY_KEYS = {
+    no: "no"
+}
+
+let dictionary = new Dictionary(/lan/no.json);
+dictionary.setLanguage("no");
+dictionary.get("end")
